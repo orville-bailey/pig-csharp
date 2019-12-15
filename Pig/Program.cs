571c689de1e0441b35e934f5fb6eba972dadfab3
Orville Bailey <orville@orvillebailey.com>
2019-12-15 11:35:58 -0500

    initial

---
+++ Pig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
